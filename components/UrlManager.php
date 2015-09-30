5b2c3e086bbefcb5877ef23b10766ee484319ae3
amnah <amnah.dev@gmail.com>
2015-09-30 01:40:33 -0400

    update UrlManager to handle regex routes

--- components/UrlManager.php
+++ components/UrlManager.php
@@ -8,25 +8,42 @@ use yii\web\UrlManager as YiiUrlManager;
 class UrlManager extends YiiUrlManager
 {
     /**
+     * @var array Regex routes that should be processed through Yii 2 instead of angular
+     */
+    public $yiiRoutes = [
+        'v\d*\/',       // api calls - v1, v2, etc
+        'debug\/*',     // debug module
+        'gii\/*',       // gii module
+    ];
+
+    /**
+     * @var string Default route to fall back on (will be processed by angular)
+     */
+    public $defaultRoute = "site/index";
+
+    /**
      * @inheritdoc
      */
-
     public function parseRequest($request)
     {
-        $validControllers = ["v1", "debug", "gii"];
-
         /** @var \yii\web\Request $request */
         $pathInfo = $request->getPathInfo();
         $params = $request->getQueryParams();
-        list($controller) = explode("/", $pathInfo);
 
-        // check if we're calling a valid controller
-        if (in_array($controller, $validControllers)) {
-            return [$pathInfo, $params];
+        // check for empty
+        if (!$pathInfo) {
+            return [$this->defaultRoute, []];
         }
 
-        // otherwise reroute to angular
-        return ["site/index", []];
+        // check if we're calling a yii route
+        foreach ($this->yiiRoutes as $yiiRoute) {
+            if (preg_match("/{$yiiRoute}/i", $pathInfo)) {
+                return [$pathInfo, $params];
+            }
+        }
+
+        // use default route
+        return [$this->defaultRoute, []];
     }
 
 }
