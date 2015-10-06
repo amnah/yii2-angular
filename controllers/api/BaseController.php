1ec89d724404826e1f4f77cfa0f80a94a0fd3877
amnah <amnah.dev@gmail.com>
2015-10-06 07:59:35 -0400

    update CORS to allow cookies

--- controllers/api/BaseController.php
+++ controllers/api/BaseController.php
@@ -34,7 +34,17 @@ class BaseController extends Controller
     public function behaviors()
     {
         $behaviors = parent::behaviors();
-        $behaviors["corsFilter"] = Cors::className();
+        $behaviors["corsFilter"] = [
+            "class" => Cors::className(),
+            "cors" => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Credentials' => true, // allow cookies
+                'Access-Control-Max-Age' => 1800, // 30 minutes
+                'Access-Control-Expose-Headers' => [],
+            ],
+        ];
         $behaviors["jwtAuth"] = Yii::$app->jwtAuth;
         return $behaviors;
     }
