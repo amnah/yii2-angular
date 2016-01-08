aab30da8c49194c3b8723291cdd4c62e43cfe516
amnah <amnah.dev@gmail.com>
2016-01-08 00:12:25 -0500

    move refresh token demo functionality from profile to index

--- web/js/auth/auth.controller.profile.js
+++ web/js/auth/auth.controller.profile.js
@@ -6,18 +6,12 @@
         .controller('ProfileCtrl', ProfileCtrl);
 
     // @ngInject
-    function ProfileCtrl($filter, $localStorage, Config, Api, Auth) {
+    function ProfileCtrl(Api) {
 
         var vm = this;
-        vm.Auth = Auth;
         vm.Profile = null;
-        vm.message = '-----';
         vm.submitting = false;
         vm.errors = {};
-        var refreshToken = Auth.getRefreshToken();
-        if (refreshToken) {
-            vm.message = $filter('date')(new Date(), 'mediumTime') + ' - Existing refresh token - ' + refreshToken.substr(-43);
-        }
 
         var apiUrl = 'user/profile';
         Api.get(apiUrl).then(function(data) {
@@ -32,28 +26,6 @@
                 vm.Profile = data.success ? data.success : vm.Profile;
                 vm.errors = data.errors ? data.errors : false;
             });
-        };
-
-        vm.requestRefreshToken = function() {
-            Auth.requestRefreshToken().then(function(data) {
-                vm.message = $filter('date')(new Date(), 'mediumTime') + ' - Got new refresh token - ' + data.success.substr(-43);
-            });
-        };
-
-        vm.removeRefreshToken = function() {
-            Auth.removeRefreshToken(true).then(function(data) {
-                vm.message = $filter('date')(new Date(), 'mediumTime') + ' - Removed refresh token';
-            });
-        };
-
-        vm.useRefreshToken = function() {
-            if (!Config.jwtCookie && !$localStorage.refreshToken) {
-                vm.message = $filter('date')(new Date(), 'mediumTime') + ' - No refresh token (using local storage)';
-            } else {
-                Auth.useRefreshToken().then(function(data) {
-                    vm.message = $filter('date')(new Date(), 'mediumTime') + ' - Used refresh token to get new regular token';
-                });
-            }
         };
     }
 
