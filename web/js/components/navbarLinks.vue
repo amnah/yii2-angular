7de3e7ab531ef35f2530658f38d0f1a40416caf7
amnah <amnah.dev@gmail.com>
2016-08-08 19:42:15 -0400

    add collapse to navbar links

--- web/js/components/navbarLinks.vue
+++ web/js/components/navbarLinks.vue
@@ -1,14 +1,14 @@
 
 <template>
     <ul class="navbar-nav navbar-right nav">
-        <router-link tag="li" to="/about" active-class="active"><a>About</a></router-link>
-        <router-link tag="li" to="/contact" active-class="active"><a>Contact</a></router-link>
-        <router-link tag="li" to="/account" active-class="active"><a>Account</a></router-link>
-        <router-link tag="li" to="/profile" active-class="active"><a>Profile</a></router-link>
-        <router-link v-if="isGuest" tag="li" to="/login"><a>Login</a></router-link>
-        <router-link v-if="isGuest" tag="li" to="/login-email"><a>Login via Email</a></router-link>
-        <router-link v-if="isGuest" tag="li" to="/register"><a>Register</a></router-link>
-        <li v-if="isLoggedIn" @click="logout"><a>Logout ({{ user.username }})</a></li>
+        <router-link tag="li" to="/about" active-class="active"><a  @click="collapse">About</a></router-link>
+        <router-link tag="li" to="/contact" active-class="active"><a  @click="collapse">Contact</a></router-link>
+        <router-link tag="li" to="/account" active-class="active"><a  @click="collapse">Account</a></router-link>
+        <router-link tag="li" to="/profile" active-class="active"><a  @click="collapse">Profile</a></router-link>
+        <router-link v-if="isGuest" tag="li" to="/login" active-class="active"><a  @click="collapse">Login</a></router-link>
+        <router-link v-if="isGuest" tag="li" to="/login-email" active-class="active"><a  @click="collapse">Login via Email</a></router-link>
+        <router-link v-if="isGuest" tag="li" to="/register" active-class="active"><a  @click="collapse">Register</a></router-link>
+        <li v-if="isLoggedIn"><a @click="logout">Logout ({{ user.email || user.username }})</a></li>
     </ul>
 </template>
 
@@ -24,6 +24,9 @@ export default {
         logout (e) {
             this.$store.dispatch('logout')
             router.push('/')
+        },
+        collapse (e) {
+            $('#navbar-collapse').collapse('hide')
         }
     }
 }
