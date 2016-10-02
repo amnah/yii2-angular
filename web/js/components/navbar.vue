7e90b0531f4bc4af80c5a5a92d95dead0fef9e6d
amnah <amnah.dev@gmail.com>
2016-10-02 17:00:31 -0400

    fix navbar collapse after vue version upgrade

--- web/js/components/navbar.vue
+++ web/js/components/navbar.vue
@@ -13,13 +13,13 @@
 
         <div id="navbar-collapse" class="collapse navbar-collapse">
             <ul class="navbar-nav navbar-right nav">
-                <router-link tag="li" to="/about" active-class="active"><a @click="collapse">About</a></router-link>
-                <router-link tag="li" to="/contact" active-class="active"><a @click="collapse">Contact</a></router-link>
-                <router-link tag="li" to="/account" active-class="active"><a @click="collapse">Account</a></router-link>
-                <router-link tag="li" to="/profile" active-class="active"><a @click="collapse">Profile</a></router-link>
-                <router-link v-if="isGuest" tag="li" to="/register" active-class="active"><a @click="collapse">Register</a></router-link>
-                <router-link v-if="isGuest" tag="li" to="/login" active-class="active" exact><a @click="collapse">Login</a></router-link>
-                <router-link v-if="isGuest" tag="li" to="/login-email" active-class="active"><a @click="collapse">Login via Email</a></router-link>
+                <router-link tag="li" to="/about" active-class="active" @click.native="collapse"><a>About</a></router-link>
+                <router-link tag="li" to="/contact" active-class="active" @click.native="collapse"><a>Contact</a></router-link>
+                <router-link tag="li" to="/account" active-class="active" @click.native="collapse"><a>Account</a></router-link>
+                <router-link tag="li" to="/profile" active-class="active" @click.native="collapse"><a>Profile</a></router-link>
+                <router-link v-if="isGuest" tag="li" to="/register" active-class="active" @click.native="collapse"><a>Register</a></router-link>
+                <router-link v-if="isGuest" tag="li" to="/login" active-class="active" exact @click.native="collapse"><a>Login</a></router-link>
+                <router-link v-if="isGuest" tag="li" to="/login-email" active-class="active" @click.native="collapse"><a>Login via Email</a></router-link>
                 <li v-if="isLoggedIn"><a @click="logout">Logout ({{ user.email || user.username }})</a></li>
             </ul>
         </div>
