b4fc57c0627b489947c9fa21af10e30f92f889c1
amnah <amnah.dev@gmail.com>
2017-01-07 22:13:15 -0500

    update yii2-app-basic

--- config/web-test.php
+++ config/web-test.php
@@ -3,4 +3,5 @@
 $config = require(__DIR__ . '/web.php');
 unset($config['components']['mailer']['transport']);
 $config['components']['db']['dsn'] .= "_test";
+$config['components']['assetManager']['basePath'] = __DIR__ . '/../web/assets';
 return $config;
