0fec9f54d8e7f86de7a9e3c8f44830de776fc7c6
amnah <amnah.dev@gmail.com>
2017-02-22 19:28:39 -0500

    update asset handling

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,35 @@
 
 'use strict';
 
-var path = require('path');
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var cssnano = require('gulp-cssnano');
-var flatten = require('gulp-flatten');
-var del = require('del');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var watchify = require('watchify');
-var gutil = require('gulp-util');
-var sourcemaps = require('gulp-sourcemaps');
-var assign = require('lodash.assign');
+const importStart = Date.now()
+const gutil = require('gulp-util');
+gutil.log(`Importing packages ...`);
+
+const path = require('path');
+const browserify = require('browserify');
+const del = require('del');
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const flatten = require('gulp-flatten');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const assign = require('lodash.assign');
+const buffer = require('vinyl-buffer');
+const source = require('vinyl-source-stream');
+const watchify = require('watchify');
+
+const importTime = (Date.now() - importStart) /  1000;
+gutil.log(`Done importing (${importTime} seconds)`);
+
+
+// -------------------------------------------------------------
+// File paths
+// -------------------------------------------------------------
+const assetPath = './assets';
+const vendorPath = assetPath + '/vendor';
+const distPath = './web/compiled';
 
 
 // -------------------------------------------------------------
@@ -22,19 +37,19 @@ var assign = require('lodash.assign');
 // https://github.com/gulpjs/gulp/blob/master/docs/recipes/fast-browserify-builds-with-watchify.md
 // https://github.com/substack/watchify
 // -------------------------------------------------------------
-var pollInterval = 350;
-var customOpts = {
-    entries: ['./web/js/main.js'],
+const pollInterval = 350;
+const customOpts = {
+    entries: [`${assetPath}/js/main.js`],
     debug: true
 };
-var browserifyOpts = assign({}, watchify.args, customOpts);
-var watchifyOpts = {poll: pollInterval, delay: pollInterval};
-var b = watchify(browserify(browserifyOpts), watchifyOpts);
-b.transform("babelify", {presets: ["es2015"]});
-b.transform("vueify");
+const browserifyOpts = assign({}, watchify.args, customOpts);
+const watchifyOpts = {poll: pollInterval, delay: pollInterval, ignoreWatch: ['**/node_modules/**', 'vendor/**', '**/*.php']};
+const b = browserify(browserifyOpts)
+    .transform("babelify", {presets: ['latest']})
+    .transform("vueify");
 
-// "Make sure to have the NODE_ENV environment variable set to "production" when building for production!
-// This strips away unnecessary code (e.g. hot-reload) for smaller bundle size"
+// Make sure to have the NODE_ENV environment variable set to "production" when building for production!
+// This strips away unnecessary code (e.g. hot-reload) for smaller bundle size.
 // @link https://github.com/vuejs/vueify#building-for-production
 process.env.NODE_ENV = 'production';
 
@@ -44,103 +59,120 @@ process.env.NODE_ENV = 'production';
 // -------------------------------------------------------------
 
 // Build task
-// create bundle directly for build (not the cached object above)
+let buildStart;
+gulp.task('default', ['build']);
 gulp.task('build', function() {
-    var bundle = browserify(browserifyOpts)
-        .transform("babelify", {presets: ["es2015"]})
-        .transform("vueify")
-        .bundle();
-    buildAll(bundle);
+    buildStart = Date.now();
+    buildAll(b.bundle());
 });
 
-// Watch task and file updates
-var theFiles;
-gulp.task('default', ['watch']);
-gulp.task('watch', function() {
-    gulp.watch('web/css/**/*.css', function(e) {
-        theFiles = [e.path];
-        buildAll(b.bundle())
-    });
-    updateBundle();
-});
-b.on('update', updateBundle);
-function updateBundle(files) {
-    theFiles = files;
+// Watch task
+// the b.on('update') is for js/vue files
+// the gulp.watch() is for sass files
+let changedFiles;
+b.on('update', function(files) {
+    changedFiles = files;
     buildAll(b.bundle());
-}
+});
+gulp.task('watch', ['build'], function() {
+    // add watchify to browserify in this task
+    watchify(b, watchifyOpts);
+    gulp.watch(`${assetPath}/sass/*`, function(e) {
+        changedFiles = [e.path];
+        buildAll(b.bundle());
+    });
+});
 
 // File change notification
+// (prep message of changed files)
 b.on('log', function(msg) {
     // get basename of files
-    var filesChanged = null;
-    if (theFiles && theFiles.join) {
-        for (var i=0; i<theFiles.length; i++) {
-            theFiles[i] = path.basename(theFiles[i]);
+    if (changedFiles && changedFiles.join) {
+        for (let i=0; i<changedFiles.length; i++) {
+            changedFiles[i] = path.basename(changedFiles[i]);
         }
-        filesChanged = theFiles.join(' / ');
+        changedFiles = changedFiles.join(' / ');
     }
-    filesChanged = filesChanged ? `${filesChanged}` : `First run`;
-    gutil.log(gutil.colors.cyan(filesChanged), msg);
+    changedFiles = changedFiles ? `${changedFiles}` : `First run`;
+    changedFiles = gutil.colors.cyan(changedFiles) + ' ' + msg;
 });
 
-
 // -------------------------------------------------------------
-// Functions
+// Build
 // -------------------------------------------------------------
 
 function buildAll(bundle) {
 
-    var dest = 'web/compiled';
-    var vendorDir = 'web/vendor';
-
     // clean files first
-    del([`${dest}/*`]).then(function() {
+    del([`${distPath}/*`]).then(function() {
 
-        // build app js files
-        var stream = bundle
+        // -----------------------------------------------------
+        // build compiled js files
+        const jsStream = bundle
             .on('error', function(err) {
-                gutil.log(gutil.colors.red("Browserify error:"), err.message);
+                gutil.log(gutil.colors.red('Browserify error:'), err.message);
+            })
+            .on('end', function() {
+                if (changedFiles) {
+                    gutil.log(changedFiles);
+                } else if (buildStart) {
+                    const buildTime = (Date.now() - buildStart) / 1000;
+                    const totalTime = (Date.now() - importStart) / 1000;
+                    gutil.log(`Bundle time (${buildTime} seconds)`);
+                    gutil.log(`Total time  (${totalTime} seconds)`);
+                }
             })
             .pipe(source('app.js'))
             .pipe(buffer());
-        stream
+        jsStream
             .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-            .pipe(sourcemaps.write('./')) // writes .map file
-            .pipe(gulp.dest(dest));
-        stream
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest(distPath));
+        jsStream
             .pipe(concat(`app.min.js`))
-            .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
             .pipe(uglify())
-            .pipe(sourcemaps.write('./')) // writes .map file
-            .pipe(gulp.dest(dest));
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest(distPath));
 
+        // -----------------------------------------------------
         // build vendor js files
-        gulp.src([`${vendorDir}/**/jquery-*.js`, `${vendorDir}/**/*.js`, `!${vendorDir}/**/*.min.js`])
+        gulp.src([`${vendorPath}/**/jquery-*.js`, `${vendorPath}/**/*.js`, `!${vendorPath}/**/*.min.js`])
             .pipe(concat(`vendor.js`))
-            .pipe(gulp.dest(dest));
-        gulp.src([`${vendorDir}/**/jquery-*.min.js`, `${vendorDir}/**/*.min.js`])
+            .pipe(gulp.dest(distPath));
+        gulp.src([`${vendorPath}/**/jquery-*.min.js`, `${vendorPath}/**/*.min.js`])
             .pipe(concat(`vendor.min.js`))
-            .pipe(gulp.dest(dest));
+            .pipe(gulp.dest(distPath));
 
-        // copy map files
-        gulp.src(`${vendorDir}/**/*.map`)
-            .pipe(flatten())
-            .pipe(gulp.dest(dest)); 
-
-        // build app css files
-        gulp.src('web/css/**/*.css')
+        // -----------------------------------------------------
+        // build compiled css files
+        const cssStream = gulp.src(`${assetPath}/sass/main.scss`)
+            .pipe(sourcemaps.init())
+            .pipe(sass().on('error', function(err) {
+                gutil.log(gutil.colors.red("Sass error:"), err.message);
+            }));
+        cssStream
             .pipe(concat(`app.css`))
-            .pipe(gulp.dest(dest))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest(distPath));
+        cssStream
             .pipe(concat(`app.min.css`))
-            .pipe(cssnano())
-            .pipe(gulp.dest(dest));
+            .pipe(cleanCSS(/*{compatibility: 'ie8'}*/))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest(distPath));
 
+        // -----------------------------------------------------
         // build vendor css files
-        gulp.src([`${vendorDir}/**/*.css`, `!${vendorDir}/**/*.min.css`])
+        gulp.src([`${vendorPath}/**/*.css`, `!${vendorPath}/**/*.min.css`])
             .pipe(concat(`vendor.css`))
-            .pipe(gulp.dest(dest));
-        gulp.src([`${vendorDir}/**/*.min.css`])
+            .pipe(gulp.dest(distPath));
+        gulp.src([`${vendorPath}/**/*.min.css`])
             .pipe(concat(`vendor.min.css`))
-            .pipe(gulp.dest(dest));
+            .pipe(gulp.dest(distPath));
+
+        // -----------------------------------------------------
+        // copy map files
+        gulp.src(`${vendorPath}/**/*.map`)
+            .pipe(flatten())
+            .pipe(gulp.dest(distPath));
     });
 }
